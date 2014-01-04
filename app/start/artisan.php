4cbf1e0e45fff73558cfbf4606018043e2fc187e
周军 <ikodota@gmail.com>
2014-01-04 21:32:12 +0800

    翻译

--- app/start/artisan.php
+++ app/start/artisan.php
@@ -3,11 +3,14 @@
 /*
 |--------------------------------------------------------------------------
 | Register The Artisan Commands
+| 注册Artisan命令
 |--------------------------------------------------------------------------
 |
 | Each available Artisan command must be registered with the console so
 | that it is available to be called. We'll register every command so
 | the console gets access to each of the command object instances.
 |
+| 每个可用的Artisan命令必须注册到Console控制台，以便它可以被调用。
+| 我们会注册每一个命令，以便在控制台获得访问每一个命令对象的实例。
 */
 
