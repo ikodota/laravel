ab2bc506ab3b23b067cd876271760c9631fe3552
周军 <ikodota@gmail.com>
2014-01-06 15:14:03 +0800

    翻译

--- app/config/database.php
+++ app/config/database.php
@@ -10,7 +10,9 @@ return array(
 	| By default, database results will be returned as instances of the PHP
 	| stdClass object; however, you may desire to retrieve records in an
 	| array format for simplicity. Here you can tweak the fetch style.
-	|
+	| 
+	| 默认情况下，数据库结果将会作为一个PHP标准对象返回，你可能希望获取到记录在一个简单的数组中
+	| 这里可以调整你获取的记录的样式。
 	*/
 
 	'fetch' => PDO::FETCH_CLASS,
@@ -24,6 +26,8 @@ return array(
 	| to use as your default connection for all database work. Of course
 	| you may use many connections at once using the Database library.
 	|
+	| 你可以在下面指定一个你希望使用的默认数据库连接，当然，
+	| 你可以可以使用许多连接来立即链接到数据库。
 	*/
 
 	'default' => 'mysql',
@@ -42,6 +46,9 @@ return array(
 	| so make sure you have the driver for your particular database of
 	| choice installed on your machine before you begin development.
 	|
+	| 这里为你的应用设置每个数据库连接，这个例子中的配置是Laravel支持，让开发起来更简单。
+	|
+	| 在Laravel中所有数据库都是通过PHP PDO，因此请确保您服务器安装有对应驱动程序。
 	*/
 
 	'connections' => array(
@@ -88,12 +95,15 @@ return array(
 	/*
 	|--------------------------------------------------------------------------
 	| Migration Repository Table
+	| 迁移存储库表
 	|--------------------------------------------------------------------------
 	|
 	| This table keeps track of all the migrations that have already run for
 	| your application. Using this information, we can determine which of
 	| the migrations on disk haven't actually been run in the database.
-	|
+	| 
+	| 这个表保存所有已运行你的应用程序的迁移轨迹。利用这些信息，
+	| 我们可以判断哪些磁盘上的迁移都没有实际的数据库上运行。
 	*/
 
 	'migrations' => 'migrations',
@@ -101,12 +111,15 @@ return array(
 	/*
 	|--------------------------------------------------------------------------
 	| Redis Databases
+	| Redis 数据库
 	|--------------------------------------------------------------------------
 	|
 	| Redis is an open source, fast, and advanced key-value store that also
 	| provides a richer set of commands than a typical key-value systems
 	| such as APC or Memcached. Laravel makes it easy to dig right in.
 	|
+	|Redis是个开源的、快速的和高级的键值存储系统，它也提供一组丰富的命令，
+	|就像APC或Memcached,Laravel让它使用起来更简单。
 	*/
 
 	'redis' => array(
